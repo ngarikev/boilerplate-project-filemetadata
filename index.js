bfd9bd389f8cbbafd276c70a1e8c10f38dec91c0
Kevin Ngari <kevin.gary191@gmail.com>
2024-12-20 18:53:46 +0000

    updated

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
+const upload = multer({dest: "uploads/"})
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+
+app.post('/api/fileanalyse', upload.single("upfile"), function(req, res) {
+  const { originalname, mimetype, size}  = req.file
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  })
+})
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
